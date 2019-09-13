1c05cbe5abeb305ec7f844ea3e64db06dfe1b67a
oladimeji-ojo <ojokuredim@gmail.com>
2019-09-13 12:20:53 +0100

    rendering tweaked

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -23,7 +23,7 @@ const App = () => {
           // debugger
           setCharacter(res.data.results);
         })
-    }, [setCharacter]);
+    }, [Character]);
 
     
   return (
--- starwars/src/CharacterList.js
+++ starwars/src/CharacterList.js
@@ -1,36 +1,37 @@
-import React, { useState, useEffect }from 'react';
-import './App.css';
-import axios from 'axios';
-import App from './App';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import App from "./App";
 
-const Character = ({name, height, mass, hairColor}) =>{
+const Character = ({ name, height, mass, hairColor, gender, films }) => {
+  return (
+    <div>
+      <span>
+        Name: {name} Mass: {height} Height: {mass} HairColor: {hairColor}
+        Gender: {gender}
+      </span>
+    </div>
+  );
+};
+
+const CharacterList = props => {
+  const { data } = props;
 
   return (
-
-        <div>
-
-          <p>Name: {name}</p>
-          <p>Mass {height}</p>
-          <p>Height {mass}</p>
-          <p>hairColor {hairColor}</p>
-        </div>
-
-  )
-
-}
-
-
-const CharacterList = (props) => {
-        const {data} = props;
-    return (
-        <div className="Character">
-          {props.data.map((el) =>{
-              return  <Character name={el.name} height={el.height} mass={el.mass} hairColor={el.hair_color}/>
-          })
-          }
-        </div>
-      );
-}
-
+    <div className="Character">
+      {props.data.map(el => {
+        return (
+          <Character
+            name={el.name}
+            height={el.height}
+            mass={el.mass}
+            hairColor={el.hair_color}
+            gender={el.gender}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CharacterList;
